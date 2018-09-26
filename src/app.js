a6448dabca9062731b65bf9c1fa7e2e99400a7dd
Adam Wells <alwells408@gmail.com>
2018-09-26 08:32:59 -0400

    Small updates

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-import '../styles/App.scss';
+import './styles/App.scss';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>
-          React
+          React Boilerplate
         </h1>
       </div>
     );
   }
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
