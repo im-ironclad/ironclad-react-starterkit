14393f0bcec7d79a9e8381ada006c672fb796a79
Adam Wells <alwells408@gmail.com>
2018-09-27 10:18:40 -0400

    Starting to implement pages/components

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Components {
+
+  render() {
+    return (
+      <h1>
+        This is the homepage
+      </h1>
+    )
+  }
+}
+
+export default Home;
