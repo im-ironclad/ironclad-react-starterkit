cb1d33957db65f790ae102b173053f291a1d80f6
Adam Wells <awells@wrayward.com>
2018-10-30 14:15:49 -0400

    Added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: path.join(__dirname, "/src/app.js"),
   output: {
     path: path.join(__dirname, "/public"),
@@ -24,5 +25,10 @@ module.exports = {
         ]
       }
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
   }
 }
